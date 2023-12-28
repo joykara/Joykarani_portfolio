32c8b35be78b1b475c6adace7adb064b4db12792
joykara <joykarani1@gmail.com>
2023-12-28 15:14:07 +0300

    edit details

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,6 @@ export { default as Header } from './header/Header';
 export { default as Footer } from './footer/Footer';
 export { default as Navbar } from './navbar/Navbar';
 export { default as About } from './about/About';
-export { default as Projects } from './projects/Projects';
+export { default as Projects } from '../pages/Projects';
 export { default as Stacks } from './stacks/Stacks';
 export { default as Contact } from './contact/Contact';
