ad50d13e98f05dd8e7981eb6f2e92fda1c28f129
joykara <joykarani1@gmail.com>
2023-12-28 15:10:15 +0300

    add project list

---
+++ src/components/projects/projectList.js
@@ -0,0 +1,24 @@
+import hemllin from '../../assets/project/Hemllin.jpeg';
+
+const projectList = [
+    {
+        id: 1,
+        title: "Management Consultancy Firm -Hemllin",
+        imgSrc: hemllin,
+        description: 'The Management Consultancy Firm Website project is a dynamic and modern web platform designed to elevate the online presence of a leading consultancy firm. This comprehensive web solution offers an array of features tailored to meet the needs of both the consultancy firm and its clients',
+        code: 'https://github.com/joykara/Hemllin-master',
+        link:'https://hemllin-master.vercel.app/'
+
+    },
+    {
+        id: 2,
+        title: "Management Consultancy Firm -Hemllin",
+        imgSrc: hemllin,
+        description: 'The Management Consultancy Firm Website project is a dynamic and modern web platform designed to elevate the online presence of a leading consultancy firm. Leveraging the power of the MERN stack—MongoDB, Express.js, React, and Node.js—this comprehensive web solution offers an array of features tailored to meet the needs of both the consultancy firm and its clients',
+        code: '',
+        link:''
+
+    }
+]
+
+export default projectList
