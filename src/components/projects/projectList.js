b1726789b577982d911a7029bfd56ee625de990c
Joy Karani <99082005+joykara@users.noreply.github.com>
2024-04-26 17:18:10 +0300

    Update projectList.js

--- src/components/projects/projectList.js
+++ src/components/projects/projectList.js
@@ -12,11 +12,11 @@ const projectList = [
     },
     {
         id: 2,
-        title: "Management Consultancy Firm -Hemllin",
-        imgSrc: hemllin,
-        description: 'The Management Consultancy Firm Website project is a dynamic and modern web platform designed to elevate the online presence of a leading consultancy firm. Leveraging the power of the MERN stack—MongoDB, Express.js, React, and Node.js—this comprehensive web solution offers an array of features tailored to meet the needs of both the consultancy firm and its clients',
-        code: '',
-        link:''
+        title: "SpeciAppt",
+        imgSrc: speciappt,
+        description: 'SpeciAppt is a dynamic and modern web platform designed to aid in scheduling and booking appointments. Leveraging the power of the MERN stack—MongoDB, Express.js, React, and Node.js—this comprehensive web solution offers an array of features tailored to meet the needs of both the patients and doctors',
+        code: 'https://github.com/joykara/SpeciAppt',
+        link:'https://speci-appt.vercel.app/'
 
     }
 ]
