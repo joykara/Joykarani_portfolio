ebbd4a1f49eae03f82b73bd79389721d695b392e
joykara <joykarani1@gmail.com>
2024-05-10 13:32:21 +0300

    add toast provider

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Homepage } from './pages';
 import { Projects, Contact } from './components';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <>
     <Router>
+      <Toaster position="top-right" reverseOrder={false} />
         <div className="App">
           <Routes>
             <Route path="/" exact element={<Homepage />} />
