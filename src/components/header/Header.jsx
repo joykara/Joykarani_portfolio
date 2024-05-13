fbdc4cf79160b5ef2274f86956101f15f76e6de0
joykara <joykarani1@gmail.com>
2024-05-13 19:23:55 +0300

    edit styling and add cv file

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,36 +3,38 @@ import './header.css';
 import { FaTwitter, FaInstagram, FaLinkedinIn, FaGithub } from 'react-icons/fa';
 import blob from '../../assets/blobanimation.svg';
 import TypewriterComponent from 'typewriter-effect';
+import CVFile from '../../assets/JoyKarani-CV.pdf'
 
 const Header = () => {
-    return (
-      <div className="header-container">
-        <img src={blob} alt="" className='blob'/>
-        <div className="header-title">
-          <h1>Joy Karani</h1>
-          {/* use TypeWriterComponent */}
-          <TypewriterComponent className="typed-text"
-            options={{
-              strings: [
-                      "Frontend Developer",
-                      "Backend Developer",
-                      "Full Stack Developer"
-              ],
-              autoStart: true,
-              loop: true,
-            }}
-          />
-          <p>Freelancer providing web development and design services. I'm always eager to learn and adapt to new technologies and industry trends.</p>
-          <div className="header-social-links">
-            <a href="https://twitter.com/kokkuri_sa" title="social"><FaTwitter color='black' size={30} /></a>
-            <a href="https://www.instagram.com/" title="social"><FaInstagram color='black' size={30} /></a>
-            <a href="https://www.linkedin.com/in/joy-k-aba15b206/" title="social"><FaLinkedinIn color='black' size={30} /></a>
-            <a href="https://github.com/joykara" title="social"><FaGithub color='black' size={30} /></a>
-          </div>
+  return (
+    <div className="header-container">
+      <img src={blob} alt="" className='blob'/>
+      <div className="header-title">
+        <h1>Joy Karani</h1>
+        {/* use TypeWriterComponent */}
+        <TypewriterComponent className="typed-text"
+          options={{
+            strings: [
+                    "Frontend Developer",
+                    "Backend Developer",
+                    "Full Stack Developer"
+            ],
+            autoStart: true,
+            loop: true,
+          }}
+        />
+        <p>A passionate developer providing web development and design services. My enthusiasm for learning drives me to stay abreast of the latest technologies and industry trends</p>
+        <div className="header-social-links">
+          <a href="https://twitter.com/kokkuri_sa" target="_blank" rel="noopener noreferrer" title="social"><FaTwitter color='black' size={30} /></a>
+          <a href="https://www.instagram.com/" title="social"><FaInstagram color='black' size={30} /></a>
+          <a href="https://www.linkedin.com/in/joy-k-aba15b206/" target="_blank" rel="noopener noreferrer" title="social"><FaLinkedinIn color='black' size={30} /></a>
+          <a href="https://github.com/joykara" target="_blank" rel="noopener noreferrer" title="social"><FaGithub color='black' size={30} /></a>
         </div>
-        <div className="header-details">
-        </div>
+      <div className="resume-box">
+          <a href={CVFile} download><button>Download CV</button></a>
       </div>
+      </div>
+    </div>
   )
 }
 
