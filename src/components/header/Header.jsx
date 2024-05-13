d9c780ff8b9a9a889a4af3ca0517bf7952d105ef
joykara <joykarani1@gmail.com>
2024-05-13 21:21:19 +0300

    remove ig

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './header.css';
-import { FaTwitter, FaInstagram, FaLinkedinIn, FaGithub } from 'react-icons/fa';
+import { FaTwitter, FaLinkedinIn, FaGithub } from 'react-icons/fa';
 import blob from '../../assets/blobanimation.svg';
 import TypewriterComponent from 'typewriter-effect';
 import CVFile from '../../assets/JoyKarani-CV.pdf'
@@ -26,7 +26,6 @@ const Header = () => {
         <p>A passionate developer providing web development and design services. My enthusiasm for learning drives me to stay abreast of the latest technologies and industry trends</p>
         <div className="header-social-links">
           <a href="https://twitter.com/kokkuri_sa" target="_blank" rel="noopener noreferrer" title="Twitter"><FaTwitter color='black' size={30} /></a>
-          {/* <a href="https://www.instagram.com/" title="social"><FaInstagram color='black' size={30} /></a> */}
           <a href="https://www.linkedin.com/in/joy-k-aba15b206/" target="_blank" rel="noopener noreferrer" title="LinkedIn"><FaLinkedinIn color='black' size={30} /></a>
           <a href="https://github.com/joykara" target="_blank" rel="noopener noreferrer" title="Github"><FaGithub color='black' size={30} /></a>
         </div>
