1a8c4a3c5d9d1a657dd9796d1ac05c84e8ef84e1
joykara <joykarani1@gmail.com>
2024-07-17 11:42:02 +0300

    chore: add @vercel/analytics for website analytics

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import './App.css';
-import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Homepage } from './pages';
-import { Projects, Contact } from './components';
-import { Toaster } from 'react-hot-toast';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Homepage } from "./pages";
+import { Projects, Contact } from "./components";
+import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
-    <Router>
-      <Toaster position="top-right" reverseOrder={false} />
+      <Router>
+        <Analytics />
+        <Toaster position="top-right" reverseOrder={false} />
         <div className="App">
           <Routes>
             <Route path="/" exact element={<Homepage />} />
@@ -16,7 +18,7 @@ function App() {
             <Route path="/contact" element={<Contact />} />
           </Routes>
         </div>
-    </Router>
+      </Router>
     </>
   );
 }
