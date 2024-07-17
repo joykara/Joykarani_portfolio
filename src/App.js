b4c4cc2fbbfaee59ad0b00224ccf17716fbd078f
joykara <joykarani1@gmail.com>
2024-07-17 12:01:42 +0300

    chore: add @vercel/speed-insights for web performance monitoring

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import { Homepage } from "./pages";
 import { Projects, Contact } from "./components";
 import { Toaster } from "react-hot-toast";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
     <>
       <Router>
         <Analytics />
+        <SpeedInsights />
         <Toaster position="top-right" reverseOrder={false} />
         <div className="App">
           <Routes>
             <Route path="/" exact element={<Homepage />} />
             <Route path="/projects" exact element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
+            <Route path="/#contact" element={<Contact />} />
           </Routes>
         </div>
       </Router>
