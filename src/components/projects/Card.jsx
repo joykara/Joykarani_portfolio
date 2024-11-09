f511deb053aa7335e83f053e1e2b8171f43579c4
joykara <joykarani1@gmail.com>
2024-11-09 17:03:52 +0300

    chore: update project cards

--- src/components/projects/Card.jsx
+++ src/components/projects/Card.jsx
@@ -7,14 +7,14 @@ const Card = () => {
   return (
     <div className='project-cards'>
     {projectList.map((project) => (
-      <div className='project-card-content scale-up'key={project.id}>
+      <div className='project-card-content scale-up rounded-md'key={project.id}>
         <img src={project.imgSrc} alt={project.title} />
         <div className="project-card-info">
           <h3>{project.title}</h3>
-          <p>{project.description}</p>
+          <p className='font-eb'>{project.description}</p>
           <div className="project-links">
-            <Link to={project.code} target="_blank" rel="noopener noreferrer">View Code</Link>
-            <a href={project.link} target="_blank" rel="noopener noreferrer">Visit Site</a>
+            <Link to={project.code} target="_blank" rel="noopener noreferrer" className='rounded-sm'>View Code</Link>
+            <a href={project.link} target="_blank" rel="noopener noreferrer" className='rounded-sm'>Visit Site</a>
           </div>
         </div>
       </div>
